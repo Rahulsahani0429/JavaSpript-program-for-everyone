1ed893260c6ba81ef4ea268ad30ad817723fd2f3
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-05 21:12:46 +0530

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,23 @@
+let num1=8;
+let num2=5;
+// addtion
+console.log("adding of two number is:",num1+num2);
+// substraction
+console.log("subtraction of two number is =",num1-num2);
+// multiple
+console.log("multiplication of two number is:",num1*num2);
+// division
+console.log("adding fo two number is:",num1/num2);
+// module means show the remainder
+
+
+console.log("molule of two number is:",num1%num2);
+// num1=1+num1
+console.log(" pre-increament of ++a:",++num1);
+// post-increament means num2=num2+1
+console.log(" post-incriament of two number is:",num2);
+// exponetial means power of any number
+
+console.log("exponetial of two number is:",num1**num2);
+
+
