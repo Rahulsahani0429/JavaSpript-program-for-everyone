dfb46baadd0417187c4930c96334bbb5a32e7217
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-06 23:15:37 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,22 @@
+// breate the object
+// let rectanlge = {
+//     length:2,
+//     breadth:4,
+//     draw: function() {
+//         console.log('draw this code');
+//     }
+// };
+// factory function
+function createRectangle() {
+return rectangle = {
+    length:3, 
+    breadth:5,
+
+     draw() {
+        console.log("use of this function we create the object");
+     }
+     
+};
+} 
+rectangle.length;
+rectangle.breadth;
