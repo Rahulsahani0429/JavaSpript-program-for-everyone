a137b87d7de6f3f0da1ddf07cc13cf06acb8116a
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-08 23:00:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("welcome to this program");
+console.log("this is "+"apple"+"set")
+let name=("rahul kumar sahani");
+console.log(name.length);
+console.log(name[17]);
+let emtpy="";
+console.log(emtpy.length);
+
+let space=" ";
+console.log(space.length);
+let num=123;
+console.log(name, "world", num,(3+6));
+let a=4;
+let b=5;
+//`` back ticks is work as the duble quote and single quote 
+
+
+console.log(`your pay ${a+b} rupees`);
