6f333c16af09edfc2eda732b9748124feee74852
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-09 20:08:29 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,17 @@
+let a=189;
+console.log("you are aboult");
+if(a>=18){
+console.log("you can vote");
+console.log("you can also drive any four willer ");
+}
+
+if(a<=15){
+    console.log("you can't vote");
+}
+let name="rahul";
+if(name=="rahul"){
+    console.log("you can do any things");
+}
+if(name!="rahul"){
+    console.log("you can meet any person");
+}
