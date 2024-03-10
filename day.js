1b8ed35ab4a6135756f9001ee10edf59f799c397
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-10 23:17:46 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,29 @@
+let day=23;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    
+    case 3:
+        console.log("wednesday");
+        break;
+        
+    case 4:
+        console.log("thrusday");
+        break;
+
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    default:
+        console.log("wrong day");
+}
