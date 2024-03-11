839cec800c2a2596a1b4e5826a2b8bada2762156
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-11 23:10:10 +0530

    Add files via upload

---
+++ swith.js
@@ -0,0 +1,16 @@
+let color="green";
+switch(color){
+    case "red":
+    console.log("stop");
+    break;
+    case "yellow":
+    console.log("drive slow");
+    break;
+    case "green":
+    console.log("go any where");
+    break;
+    default:
+        console.log("not allow to anyone");
+        break;
+
+}
