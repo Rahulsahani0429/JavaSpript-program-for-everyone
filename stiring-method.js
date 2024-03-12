6ea80dbd3267c590ef58c74f010c1c093d28ba6d
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-12 23:30:50 +0530

    Add files via upload

---
+++ stiring-method.js
@@ -0,0 +1,18 @@
+// // let name="HELLO NODE sachin";
+// // console.log(name.toUpperCase());
+// // console.log(name.toLowerCase());
+// let str="ILOVECODING";
+// console.log(str.indexOf("LOVE"));
+let sms="   hello rahul KUMAR     ";
+// let newSMS=sms.trim();
+// console.log(" after trim:",newSMS);
+// newSMS=newSMS.toUpperCase();
+// console.log(" after uppercase:",newSMS);
+// newSMS=newSMS.toLowerCase();
+// console.log("after lowercase",newSMS);
+// message trim means in one line multimple properties use 
+// propeties always  apply left to right
+
+
+let newSMS=sms.trim().toUpperCase().toLowerCase();
+console.log(newSMS);
