8d48eb702ae30b815e114445002e16c0737db1c3
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-13 23:05:01 +0530

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,23 @@
+let student=['rahul','kumar','sahani','santosh'];
+// console.log(student);
+// console.log(student[2]);
+// // typeof show the type of array and array is object type
+// console.log(typeof student);
+// console.log(student[1][0]);
+//     console.log(student[2]="sachin");
+//  console.log(student);
+// //  push means add the element at end of array
+//  console.log(student.push("sandeep"));
+// student.push("manish");
+//  console.log(student);
+// //  pop means delet the element at array
+//  student.pop("manish");
+//  console.log(student);
+//  console.log(student);
+//  shift means add the element at start or beging of array
+student.shift("sanjay");
+console.log(student);
+
+ 
+
+
