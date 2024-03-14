723b95f79d64c926476132623193bbf703527797
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-03-14 20:09:27 +0530

    Add files via upload

---
+++ nested.js
@@ -0,0 +1,24 @@
+let marks=40;
+if(marks>=50){
+    console.log("pass");
+    if(marks>=90){
+        console.log("Grade: O");
+
+    }
+    else if(marks>=80){
+        console.log("grade: A++");
+    }
+    if(marks>=70){
+        console.log("Grade: A+");
+
+    }
+    else{
+        console.log("grade: B");
+    }
+    
+    
+
+}
+else{
+    console.log("fail",`better to nest time`);
+}
