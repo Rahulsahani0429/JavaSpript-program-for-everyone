65e8c970572e2c226282d8bfc35f61b37a8e1708
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-09 20:48:13 +0530

    Add files via upload

---
+++ odd.js
@@ -0,0 +1,12 @@
+// //how to write the odd number
+// // for(let x=15; x>=1; x=x-2){
+    
+// //     console.log(x);
+
+// // }
+// // right way
+for(let x=1; x<=15; x=+2){
+    
+    console.log(x);
+
+}
---
+++ odd1.js
@@ -0,0 +1,18 @@
+// odd number
+for(let i=1; i<=100; i=i+2){
+    console.log(i);
+}
+// even number
+for(let i=0; i<=100; i=i+2){
+    console.log(i);
+}
+// reverse even number
+for(let i=100; i>=2; i=i-2){
+    console.log(i);
+
+}
+// reverse odd number
+for(let i=99; i>=1; i=i-2){
+    console.log(i);
+
+}
