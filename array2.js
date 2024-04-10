63d62727e2cff6799b15f79d6703e169018062a8
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-10 23:31:16 +0530

    Add files via upload

---
+++ array2.js
@@ -0,0 +1,9 @@
+// let fruits=["apple", "banana","guava","lichi","mango"];
+// for(let i=0; i<fruits.length; i++){
+//     console.log(i,fruits[i]);
+// }
+ // if we want to print the odd position then we use
+ let fruits=["apple", "banana","guava","lichi","mango"];
+ for(let i=0; i<fruits.length; i=i+2){
+   console.log(i,fruits[i]);
+ }
---
+++ brake1.js
@@ -0,0 +1,11 @@
+let x=1;
+while(x<=10){
+    if(x==6){
+        break;
+
+
+    }
+    
+    console.log(x);
+    x++;
+}
