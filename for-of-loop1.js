6b086448b2caa0f75fafac5f752d235ef9b2b153
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-11 23:09:52 +0530

    Add files via upload

---
+++ for-of-loop1.js
@@ -0,0 +1,12 @@
+// let fruits=['apple','banana','mango','orange'];
+// for(fruit of fruits){
+//     console.log(fruit);
+
+// }
+let name=[['pandit','vikash','vicky'],['chhoti','mansha','priti']];
+for(names of name){
+    // console.log(names);
+    for(list of names){
+        console.log(list);
+    }
+}
