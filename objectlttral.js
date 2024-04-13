796ea60aacc5a9b1362badd9420e050cb947ce52
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-13 23:17:59 +0530

    Add files via upload

---
+++ objectlttral.js
@@ -0,0 +1,17 @@
+const st={
+    name:"rahul",
+    age:23,
+    makes:98
+    
+
+    
+
+};
+const item= {
+    price:100.44,
+    discount:50,
+     colors:['red','pink']
+
+};
+// array less clarity than the object litral
+let arr=["rahul",23,94.5];
