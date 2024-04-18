6add240376f20d20f3cc7f746ec9d7b306958e9c
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-18 22:18:05 +0530

    Add files via upload

---
+++ filter1.js
@@ -0,0 +1,8 @@
+let num=[3,1,5,6,8,10,7,16];
+let ans=num.filter((get)=>{
+    // for odd
+    return get % 2 !=0;
+    
+    //return get%2==0;
+    //if condition is true then it retunt the true otherwise it retunt the false value
+});
