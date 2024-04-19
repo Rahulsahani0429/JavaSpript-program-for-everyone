53993631ef384d796288b6c5dc518a446fab4cab
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-19 21:02:08 +0530

    Add files via upload

---
+++ destructing1.js
@@ -0,0 +1,3 @@
+let names=["apple","banana","mango","lichi","orange","pineapple","pomigranet"];
+let [produce,company,farmer,...others]=names;
+// here others works as the rest keys
---
+++ destructuing-object.js
@@ -0,0 +1,10 @@
+let employ={
+    name:"prashant",
+    id:887,
+    company:"LNT",
+    salary:79879,
+    city:"mumbai",
+    happy:"no",
+
+}
+let {salary:get,company}=employ;
