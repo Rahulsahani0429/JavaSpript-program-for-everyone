8e55f7e270123fc4d4f74a8c8c919eb46ff97a95
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-21 22:44:20 +0530

    Add files via upload

---
+++ ancor1.js
@@ -0,0 +1,17 @@
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector('p'));
+// // console.dir(document.querySelector('#rahul'));
+// // console.dir(document.querySelector(".oldimg"));
+// let heading=document.querySelector("h1");
+// console.log(heading.innerText);
+
+// console.log(heading.innerText);
+
+// let yes=document.querySelector('.info a');
+// for(let i=0; i<yes.lenght; i++){
+// yes[i].style.color="yellow"
+// }
+let links=document.querySelector('info a');
+for(let i=0; links.lenght; i++){
+    links[i].style.color="purpule";
+}
