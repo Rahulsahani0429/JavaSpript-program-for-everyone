4f85c2b32d7bd5e8c2608990abb2adc1cfb0ffaf
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-25 18:48:00 +0530

    Add files via upload

---
+++ spread-array.js
@@ -0,0 +1,8 @@
+let arr=[3,2,5,6];
+let newArr=[...arr];
+console.log(newArr.push(43));
+console.log(arr);
+// if we want to include in one propeties;
+let num1=[1,3,5,7,9];
+let num2=[2,4,6,8,10]
+console.log(...num1,...num2);
