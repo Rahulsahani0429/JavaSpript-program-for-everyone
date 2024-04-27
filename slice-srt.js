67594afad354b78edb293646a559307c50926f69
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-27 21:39:01 +0530

    Add files via upload

---
+++ slice-srt.js
@@ -0,0 +1,9 @@
+let str="I love you janu";
+// slice method use to taking the substring between string 
+console.log(str.slice(2));
+ console.log(str.slice(2,4));
+console.log(str.slice(-1,-3));
+// replace method
+console.log(str.replace("love","am"));
+// repeat method
+console.log(str.repeat(3))
