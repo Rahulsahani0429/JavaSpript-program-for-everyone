856e5dd38b72ee54828dc9926f3457b2a86611bb
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-29 21:17:20 +0530

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,11 @@
+// alert("this is simple alert");
+// console.log("this is simple log msg");
+// console.error("this is a error sma");
+// console.warn("this is a warnig sms");
+
+
+let firstName= prompt("enter the first name");
+// console.log("firstName");
+let lastName= Prompt("enter the second name");
+// console.log(second);
+console.log("welcome", firstName, lastName, "!");
