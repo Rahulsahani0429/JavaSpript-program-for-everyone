821f797830ab9250da6a51b1767238a72ce3297d
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-04-30 18:37:49 +0530

    Add files via upload

---
+++ elseIf.js
@@ -0,0 +1,12 @@
+let salary=1400;
+if(salary>=15000){
+    console.log("you can travel with family");
+
+}
+else if(salary>=10000){
+    console.log("you can travel only");
+
+}
+else if(salary<=5000){
+    console.log("you can't travel");
+}
