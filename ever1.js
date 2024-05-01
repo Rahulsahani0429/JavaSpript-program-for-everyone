a6df201e6176c0df6ab027fbbef23be6db4b0b23
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-05-01 20:46:59 +0530

    Add files via upload

---
+++ ever1.js
@@ -0,0 +1,10 @@
+let num=[4,2,6,8];
+let val=num.every((el)=>{
+    return el%2==0;
+});
+let num1=[13,17,7,3,2];//it provite the false because one value is even 2
+let val1=num1.every((stu) =>{
+    return stu%2!=0;
+
+});
+    
