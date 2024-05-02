08a3b3fea0c3649a426dcb8980e60b56ecf76334
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-05-02 20:12:14 +0530

    Add files via upload

---
+++ concatinate.js
@@ -0,0 +1,8 @@
+let color=["blue", 'green','red', 'violate','yellow' ];
+let color1=['rahul','sandeep','manish', 'green'];
+// console.log(color.concat(color1));
+let colors=color.concat(color1);
+console.log(colors);
+// reverse method use to reverse the element
+console.log(color.reverse());
+
---
+++ coovertobj.js
@@ -0,0 +1,8 @@
+const  stu={
+    1:"a",
+    2:"b",
+    true:"rahul",
+    null:"d",
+    undefined:"kumar"
+    
+};
